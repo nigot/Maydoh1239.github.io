c3c27b28d735a010afe2f5632479ab1b934d084c
Maydoh1239 <edl.623.003@gmail.com>
2017-05-20 17:26:43 -0700

    civil war footer header

---
+++ civilwar/footer.js
@@ -0,0 +1,5 @@
+document.getElementById("footer").innerHTML =
+    '<div class="footer">' +
+        '<p>Copyright © 2017 – All Rights Reserved – <a target="https://github.com/maydoh1239">Maydoh1239</a> - <a href="#top">Go to Top</a></p>' +
+		'<p><a href="http://bogburg.weebly.com/uploads/1/5/4/0/15408648/1063608_orig.jpg">Background Image</a></p>' +
+    '</div>';
