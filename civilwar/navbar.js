3d4ec8319bdccb2dff1f4a28f03b92a66344e431
Maydoh1239 <edl.623.003@gmail.com>
2017-05-20 17:28:55 -0700

    navbar test

--- civilwar/navbar.js
+++ civilwar/navbar.js
@@ -27,6 +27,7 @@ document.getElementById("top").innerHTML =
 
 var linkz = document.getElementsByClassName("topnav")[0].children;
 for (var i = 0; i < linkz.length; i++) {
+    alert(linkz[i].href)
 	if (linkz[i].href === window.location.href) {
 		linkz[i].classList.add('active');
 	}
