e43a7581a577097c494dab5ebf6d739a6d0069af
Maydoh1239 <edl.623.003@gmail.com>
2017-05-21 08:54:43 -0700

    navbar fix spirals ado

--- navbar.js
+++ navbar.js
@@ -17,7 +17,7 @@ document.getElementById("top").innerHTML =
 var linkz = document.getElementsByClassName("topnav")[0].children;
 
 for (var i = 0; i < linkz.length; i++) {
-    if (linkz[i].href === window.location.href) {
+    if (linkz[i].href.replace(".html", "") === window.location.href.replace("#top", "").replace(".html", "")) {
         linkz[i].classList.add('active');
     } else if (linkz[i].className === "dropdown") {
         if (linkz[i].children[0].href === window.location.href){
